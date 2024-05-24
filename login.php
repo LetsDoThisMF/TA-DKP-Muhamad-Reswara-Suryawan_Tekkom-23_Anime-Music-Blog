311a4eff96b3fd6f7fb6155bc9bc1df3ba32299c
LetsDoThisMF <165355617+LetsDoThisMF@users.noreply.github.com>
2024-05-24 23:36:32 +0700

    Update login.php

--- login.php
+++ login.php
@@ -7,36 +7,52 @@
             echo "<p>Welcome " . $_SESSION["useruid"] . "</p>";
             }
      ?>
+<?php
+
+class User {
+    public $username;
+    public $password;
+
+    public function __construct($username, $password) {
+        $this->username = $username;
+        $this->password = $password;
+    }
+}
+
+include_once 'header.php';
+
+session_start();
+
+if(isset($_SESSION["userid"])){
+    echo "<p>Welcome " . $_SESSION["useruid"] . "</p>";
+}
+
+if (isset($_SESSION['username'])) {
+    header("Location: welcome.php");
+    exit();
+}
+
+if (isset($_POST['submit'])) {
+    $user = new User($_POST['username'], $_POST['password']);
+    $valid_user = new User("LetsDoThis", "Reza2005");
+    if ($user->username == $valid_user->username && $user->password == $valid_user->password) {
+        $_SESSION['username'] = $user->username;
+        header("Location: welcome.php");
+        exit();
+    } else {
+        echo "<p style='color:red;'>Invalid username or password</p>";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Login</title>
 </head>
-<body class ="login">
+<body class="login">
     <h2>Login</h2>
-    <?php
-    session_start();
-    if (isset($_SESSION['username'])) {
-        header("Location: welcome.php");
-        exit();
-    }
-    if (isset($_POST['submit'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        // Hard-coded credentials
-        $valid_username = "LetsDoThis";
-        $valid_password = "Reza2005";
-
-        if ($username == $valid_username && $password == $valid_password) {
-            $_SESSION['username'] = $username;
-            header("Location: welcome.php");
-            exit();
-        } else {
-            echo "<p style='color:red;'>Invalid username or password</p>";
-        }
-    }
-    ?>
     <form method="post" action="login.php">
         Username: <input type="text" name="username" required><br>
         Password: <input type="password" name="password" required><br>
